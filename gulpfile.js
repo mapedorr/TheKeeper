fb4dd57817403b465d410a09416b65b3cb2676e5
jefree <jgarzon920429@gmail.com>
2015-09-13 12:08:11 +0200

    create minify version

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,23 @@ var uglify = require('gulp-uglify');
 
 var files = [
   './js/states/start.js',
-  './js/states/intro.js',
   './js/states/lvl1.js',
-  './js/states/level.js',
+  './js/states/lvl2.js',
+  './js/states/lvl3.js',
+  './js/states/ready.js',
+  './js/states/intro.js',
+  './js/states/lvlgen.js',
+  './js/states/skills.js',
+
+  './js/game13k.js',
   './js/utils.js',
   './js/Circle.js',
-  './js/game13k.js'
+  './js/level.js'
 ];
 
 gulp.task('minjs', function(){
   return gulp.src(files)
-    .pipe(concat('js13k.js'))
+    .pipe(concat('keeper.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./dist/'));
 });
